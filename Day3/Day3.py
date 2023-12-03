dcd339f053260ea18020f6fc70b49d3a714b7863
ACOJO5920 <andrei85@hotmail.co.uk>
2023-12-03 09:47:39 +0000

    day 3 part 2

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -4,18 +4,39 @@ import string
 def main(filename):
     with open(filename) as file:
         iy = 0
-        items = []
+        nums = []
         adjacent_locations = []
         for line in file.readlines():
-            items.extend(get_nums(line, iy))
+            nums.extend(get_items(line, iy)[0])
             adjacent_locations.extend(get_adjacent_locations(line, iy))
             iy += 1
-    return calculate_sum(items, adjacent_locations)
+    return calculate_sum(nums, adjacent_locations)
 
 
-def get_nums(line, iy):
+def main_2(filename):
+    with open(filename) as file:
+        iy = 0
+        nums = []
+        gears = []
+        adjacent_locations = []
+        for line in file.readlines():
+            line_nums, line_gears = get_items(line, iy)
+            nums.extend(line_nums)
+            gears.extend(line_gears)
+            adjacent_locations.extend(get_adjacent_locations(line, iy))
+            iy += 1
+
+    total_sum = 0
+    for gear in gears:
+        total_sum += calculate_gear_ratio(gear, nums)
+
+    return total_sum
+
+
+def get_items(line, iy):
     tracking_num = False
-    items = []
+    gears = []
+    nums = []
     super_value = ""
     super_locations = []
     for ix in range(len(line)):
@@ -29,11 +50,45 @@ def get_nums(line, iy):
             super_locations.append((ix, iy))
         elif not value.isdigit() and tracking_num:
             tracking_num = False
-            items.append(Item(int(super_value), super_locations))
+            nums.append(Item(int(super_value), super_locations))
             super_value = ""
             super_locations = []
+            if value == "*":
+                gears.append((ix, iy))
+        elif value == "*":
+            gears.append((ix, iy))
 
-    return items
+    return nums, gears
+
+
+def calculate_gear_ratio(gear, nums):
+    adjacent_locations = []
+    ix, iy = gear
+    adjacent_locations.append((ix-1, iy-1))
+    adjacent_locations.append((ix, iy-1))
+    adjacent_locations.append((ix+1, iy-1))
+    adjacent_locations.append((ix-1, iy))
+    adjacent_locations.append((ix+1, iy))
+    adjacent_locations.append((ix-1, iy+1))
+    adjacent_locations.append((ix, iy+1))
+    adjacent_locations.append((ix+1, iy+1))
+
+    close_nums = 0
+    total_sum = 1
+
+    for num in nums:
+        num_locations = num.get_locations()
+        num_found = False
+        for num_location in num_locations:
+            if num_location in adjacent_locations and not num_found:
+                total_sum *= num.get_data()
+                num_found = True
+                close_nums += 1
+
+    if close_nums == 2:
+        return total_sum
+
+    return 0
 
 
 def get_adjacent_locations(line, iy):
--- Day3/TestMain.py
+++ Day3/TestMain.py
@@ -1,5 +1,5 @@
 import unittest
-from Day3 import main
+from Day3 import main, main_2
 
 
 class MyTestCase(unittest.TestCase):
@@ -9,11 +9,12 @@ class MyTestCase(unittest.TestCase):
     def test_main_input_1(self):
         self.assertEqual(522726, main('Day3Input1.txt'))
 
-    # def test_main_2_test_2(self):
-    #     self.assertEqual(2286, main('Day3Test2.txt'))
-    #
-    # def test_main_2_input_1(self):
-    #     self.assertEqual(2286, main('Day3Input1.txt'))
+
+    def test_main_2_test_2(self):
+        self.assertEqual(467835, main_2('Day3Test2.txt'))
+
+    def test_main_2_input_1(self):
+        self.assertEqual(81721933, main_2('Day3Input1.txt'))
 
 
 if __name__ == '__main__':
