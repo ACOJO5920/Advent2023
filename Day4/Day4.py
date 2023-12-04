8bd815c1ad9bcc49e3a50166a2c0d581ec3a5ac3
ACOJO5920 <andrei85@hotmail.co.uk>
2023-12-04 08:33:25 +0000

    part 2 some

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -41,12 +41,21 @@ def main_2(filename):
         for ix in range(len(cards)):
             card, num_set = cards[ix], nums[ix]
             card_numbers = card.get_numbers()
-            print(card_numbers)
 
-            won = True
             copy_cards = []
-            while won:
 
+            score = 0
+            for number in card_numbers:
+                if number.isdigit():
+                    if number in num_set:
+                        score += 1
+                        won = True
+            print(score)
+
+            while won:
+                for x in range(ix, ix+score+1):
+                    copy_cards.append(cards[x])
+                print(copy_cards)
                 score = 0
                 won = False
                 for number in card_numbers:
@@ -55,8 +64,7 @@ def main_2(filename):
                             score += 1
                             won = True
 
-                for x in range(score):
-                    copy_cards.append(cards[x])
+
 
                 print(copy_cards)
 
