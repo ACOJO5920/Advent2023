d01b361263ed807a048c9ed038c61c0fff1ddac6
ACOJO5920 <andrei85@hotmail.co.uk>
2023-12-06 08:27:05 +0000

    complete day 6
    prep day 7

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -1,4 +1,21 @@
 
 def main(filename):
     with open(filename) as file:
-        pass
+        lines = file.read().split('\n')
+        times = list(map(int, lines[0].split(":")[1].split()))
+        distances = list(map(int, lines[1].split(":")[1].split()))
+        race_time = int("".join(str(time) for time in times))
+        race_distance = int("".join(str(distance) for distance in distances))
+
+
+        ways_to_win = 0
+
+        for seconds in range(0, race_time+1):
+            seconds_after_pressing = race_time - seconds
+            initial_speed = seconds
+            distance = initial_speed * seconds_after_pressing
+            if distance > race_distance:
+                ways_to_win += 1
+
+        return ways_to_win
+
--- Day7/TestMain.py
+++ Day7/TestMain.py
@@ -1,13 +1,13 @@
 import unittest
-from Day6 import main
+from Day7 import main
 
 
 class MyTestCase(unittest.TestCase):
     def test_main_test_1(self):
-        self.assertEqual(288, main('Day6Test1.txt'))
+        self.assertEqual(288, main('Day7Test1.txt'))
 
-    # def test_main_input_1(self):
-    #     self.assertEqual(0, main('Day6Input1.txt'))
+    def test_main_input_1(self):
+        self.assertEqual(0, main('Day7Input1.txt'))
 
     # def test_main_2_test_2(self):
     #     self.assertEqual(30, main_2('Day5Test2.txt'))
